930c524130f95e90cf27ed6f2591aa1d62084fd4
HassanMirza123 <hassanm9104@gmail.com>
2026-09-14 18:57:50 +0100

    Updated DB URL

--- load_db.py
+++ load_db.py
@@ -8,6 +8,7 @@ load_dotenv()
 DB_URL = (
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    "?sslmode=require"
 )
 engine = create_engine(DB_URL)
 
